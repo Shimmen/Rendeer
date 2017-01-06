728b4d8c5f5fb296b38c9def7336ca558b7ece50
Simon Moos <simon.moos95@gmail.com>
2017-01-06 22:50:39 +0100

    Increase the exposure so that it's not so dark

--- assets/shaders/Filtering/ToneMapping.glsl
+++ assets/shaders/Filtering/ToneMapping.glsl
@@ -13,14 +13,17 @@ vec3 simpleReinhardToneMap(in vec3 hdrColor)
 
 vec3 uncharted2_ToneMap(in vec3 hdrColor)
 {
-    const float A = 0.15;
+	const float A = 0.15;
 	const float B = 0.50;
 	const float C = 0.10;
 	const float D = 0.20;
 	const float E = 0.02;
 	const float F = 0.30;
 
-    return ((hdrColor * (A * hdrColor + C * B) + D * E) / (hdrColor * (A * hdrColor + B) + D * F)) - E / F;
+	const vec3 exposureBias = vec3(3.0);
+	hdrColor *= exposureBias;
+
+	return ((hdrColor * (A * hdrColor + C * B) + D * E) / (hdrColor * (A * hdrColor + B) + D * F)) - E / F;
 }
 
 #endif // _TONE_MAPPING_GLSL
